0cb05c78750f1f9f9a6e20f5e575d9f0c5da3678
Chris Hatch <hatch@tuta.io>
2018-07-07 18:53:32 +0700

    switch to persistent db and fix how the handle is fetched

--- index.js
+++ index.js
@@ -1,19 +1,16 @@
-var express = require('express')
-var dbadapter = require('./dbadapter')
-var inmemorydbadapter = require('./inmemorydbadapter')
+const express = require('express')
+const dbadapter = require('./dbadapter')
+
+const db = new dbadapter()
 
 var app = express()
 
 // TODO: add AUTH here
 
+// TODO: configure CORS
+
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
-
-function getDBAdapter(req) {
-  //var db = new dbadapter();
-  var db = new inmemorydbadapter(req.session)
-  return db
-}
 
 function sendJsonResult(res, obj) {
   res.setHeader('Content-Type', 'application/json')
@@ -21,14 +18,12 @@ function sendJsonResult(res, obj) {
 }
 
 app.get('/getActive', function(req, res) {
-  var db = getDBAdapter(req)
   db.getSurveys(function(result) {
     sendJsonResult(res, result)
   })
 })
 
 app.get('/getSurvey', function(req, res) {
-  var db = getDBAdapter(req)
   var surveyId = req.query['surveyId']
   db.getSurvey(surveyId, function(result) {
     sendJsonResult(res, result)
@@ -36,7 +31,6 @@ app.get('/getSurvey', function(req, res) {
 })
 
 app.get('/changeName', function(req, res) {
-  var db = getDBAdapter(req)
   var id = req.query['id']
   var name = req.query['name']
   db.changeName(id, name, function(result) {
@@ -45,7 +39,6 @@ app.get('/changeName', function(req, res) {
 })
 
 app.get('/create', function(req, res) {
-  var db = getDBAdapter(req)
   var name = req.query['name']
   db.addSurvey(name, function(result) {
     sendJsonResult(res, {Name: result.name, Id: result.name})
@@ -53,7 +46,6 @@ app.get('/create', function(req, res) {
 })
 
 app.post('/changeJson', function(req, res) {
-  var db = getDBAdapter(req)
   var id = req.body.Id
   var json = req.body.Json
   db.storeSurvey(id, json, function(result) {
@@ -62,7 +54,6 @@ app.post('/changeJson', function(req, res) {
 })
 
 app.post('/post', function(req, res) {
-  var db = getDBAdapter(req)
   var postId = req.body.postId
   var surveyResult = req.body.surveyResult
   db.postResults(postId, surveyResult, function(result) {
@@ -71,7 +62,6 @@ app.post('/post', function(req, res) {
 })
 
 app.get('/delete', function(req, res) {
-  var db = getDBAdapter(req)
   var surveyId = req.query['id']
   db.deleteSurvey(surveyId, function(result) {
     sendJsonResult(res, {})
@@ -79,7 +69,6 @@ app.get('/delete', function(req, res) {
 })
 
 app.get('/results', function(req, res) {
-  var db = getDBAdapter(req)
   var postId = req.query['postId']
   db.getResults(postId, function(result) {
     sendJsonResult(res, result)
